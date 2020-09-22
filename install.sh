a7f8cd183056b066be271201606baf7aa1339126
Fraasi <doofassi@gmail.com>
2020-09-22 21:57:37 +0300

    add copy_bin_to_HOME_bin functionality

--- install.sh
+++ install.sh
@@ -24,8 +24,8 @@ fail() {
 setup_gitconfig() {
   info 'setup gitconfig, press enter to keep old ones\n'
 
-  old_git_username=$(git config user.name)
-  old_git_useremail=$(git config user.email)
+  old_git_username=$(git config --global user.name)
+  old_git_useremail=$(git config --global user.email)
 
   user "- New github user name? [$old_git_username]"
   read -re new_git_username
@@ -38,24 +38,47 @@ setup_gitconfig() {
   success 'gitconfig'
 }
 
-copy_files_to_HOME() {
-  info 'copy Git-Bash/ files to  ~/\n'
+copy_Git_Bash_to_HOME() {
+  info 'copy Git-Bash/ files to ~/\n'
 
   for file in $(find ./Git-Bash -type f); do
     filename=$(basename "$file")
     cp -v --interactive "$file" "$HOME/$filename"
   done
   echo ''
-  success 'copy files'
+  success 'copied files'
 }
 
-select choise in exit setup_gitconfig copy_files_to_HOME
-do
+copy_bin_to_HOME_bin() {
+  info 'copy bin/ files to ~/bin to put them in PATH\n'
+
+  if ! [[ -e ~/bin && -d ~/bin ]]; then
+    info 'No ~/bin folder, creating one...'
+    echo ''
+    mkdir ~/bin
+  fi
+
+  for file in $(find ./bin -type f); do
+    filename=$(basename "$file")
+    cp -v --interactive "$file" "$HOME/bin/$filename"
+  done
+  echo ''
+  success 'copied files'
+}
+
+choices='exit setup_gitconfig copy_Git_Bash_to_HOME copy_bin_to_HOME_bin'
+
+select choise in ${choices}; do
   case $REPLY in
-    1) user "$choise"; break ;;
-    2) ( "$choise" ) ;;
-    3) ( "$choise" ) ;;
+  1)
+    user "$choise"
+    break
+    ;;
+  2) ("$choise") ;;
+  3) ("$choise") ;;
+  4) ("$choise") ;;
   esac
 done
 
-info "All done, run 'exec bash' to reflash current shell."
+info "All done, run 'source ~/.profile' to reflash current prompt."
+info "Or 'exec bash' to update PATH."

8e6c8733868ab82f3ec20faf095c5fe67aa6e4d3
Fraasi <doofassi@gmail.com>
2020-12-16 23:33:21 +0200

    feat: dir-size func

--- Git-Bash/functions.sh
+++ Git-Bash/functions.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+# shellcheck disable=SC2012,SC2010
 
 # Changing directory to Code/
 function cdir() {
@@ -14,7 +15,7 @@ function pdtapp() {
 }
 
 # Copy all files from subdirs to ./All-Files/
-copy-all-to-all() {
+function copy-all-to-all() {
   read -rp "Are you sure? (don't do it in a large folder/root) (Y/n) " ANSWER
   case ${ANSWER:0:1} in
     Y )
@@ -39,7 +40,7 @@ copy-all-to-all() {
 
 
 # print files in folders in a tree
-print-tree() {
+function print-tree() {
   if [ "$1" = "--write" ]; then
     find . -not -path './node_modules/*' -not -path './.git/*' | sed -e 's/:$//' -e 's/[^-][^\/]*\// /g' -e 's/^/ /' > Tree_listing-"$(date +'%Y_%m_%d')".txt
     echo "Tree written to file Tree_listing-$(date +'%Y_%m_%d').txt"
@@ -49,7 +50,7 @@ print-tree() {
 }
 
 # print all color codes in color (max NUM=255), can pipe to column. https://unix.stackexchange.com/questions/269077/tput-setaf-color-table-how-to-determine-color-codes
-print-colors() {
+function print-colors() {
   NUM=${1:-15}
   for (( c=0; c<="$NUM"; c++ ))
   do
@@ -61,6 +62,12 @@ print-colors() {
 }
 
 # show my ip address
-show-ip() {
+function show-ip() {
   curl ifconfig.me
+}
+
+# show filecount total size
+function dir-size() {
+  echo "$(ls -1ah | wc -l) files, $(ls -lah | grep -m 1 total | sed 's/tot
+al //')b"
 }

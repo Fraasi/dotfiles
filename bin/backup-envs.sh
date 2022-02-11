b2bea35d2db0cb270876b2ae1978d61f063741b7
fraasi <doofassi@gmail.com>
2022-02-11 15:13:18 +0200

    add date to filename

--- bin/backup-envs.sh
+++ bin/backup-envs.sh
@@ -4,7 +4,8 @@
 # Find all env files & put their content in envs.txt file
 # & optionally encrypt the file for backup purposes
 
-ENV_FILES=$(find . -maxdepth 2 -regex '.*\(.env\|env.js\)$')
+ENV_FILES=$(find . -maxdepth 2 -regextype posix-extended -regex '.*(.env(.local)?|env.js)$')
+SAVE_FILE="envs_$(date '+%d-%m-%Y').txt"
 
 if [[ -z "$ENV_FILES" ]]; then
   echo -e '\nNo env files found with -maxdepth 2 from: ' "$PWD"
@@ -12,13 +13,13 @@ if [[ -z "$ENV_FILES" ]]; then
 fi
 
 echo -e '\nFound env files at -maxdepth 2\n'
-echo "$ENV_FILES" | xargs -n1
+echo "$ENV_FILES"
 echo ''
 
 read -r -p "Continue (y/n) " CONT
 case $CONT in
 y)
-  echo 'Writing to envs.txt...'
+  echo "yes"
   ;;
 *)
   echo 'Canceling...'
@@ -27,34 +28,34 @@ y)
 esac
 
 {
-  echo -e '\n***********************\n'
+  echo -e '***********************\n'
   date '+%d-%m-%Y %T'
   echo -e '\n***********************\n'
-} >> envs.txt
+} > "$SAVE_FILE"
 
 for FILE in $ENV_FILES; do
   {
     echo "$FILE"
     echo ''
     cat "$FILE"
-  } >> envs.txt
-  echo -e '\n--------------------------\n' >> envs.txt
+  } >> "$SAVE_FILE"
+  echo -e '\n--------------------------\n' >> "$SAVE_FILE"
 done
 
-echo 'envs.txt file created'
+echo "$SAVE_FILE file created"
 echo ''
 
-read -r -p "Encrypt envs.txt file? (y/n) " GPG
+read -r -p "Encrypt $SAVE_FILE file? (y/n) " GPG
 case ${GPG:0:1} in
 y)
-  gpg -c --no-symkey-cache envs.txt
+  gpg -c --no-symkey-cache "$SAVE_FILE"
   echo 'Gpg file created'
   echo ''
-  read -r -p "Delete envs.txt file? (y/n) " DELETE
+  read -r -p "Delete .txt file? (y/n) " DELETE
   case ${DELETE:0:1} in
   y)
-    rm envs.txt
-    echo 'envs.txt deleted'
+    rm "$SAVE_FILE"
+    echo "$SAVE_FILE deleted"
     ;;
   *)
     echo 'no'
@@ -70,4 +71,4 @@ esac
 echo ''
 echo 'env files in folder:'
 echo ''
-ls ./*env*
+ls -1 ./*env*

065814ca98c4fdc6119ba442dd95fffb54f95e59
fraasi <doofassi@gmail.com>
2022-04-01 23:02:35 +0300

    rewrite using find & log file

--- bin/backup-phone-photos.sh
+++ bin/backup-phone-photos.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
-#shellcheck disable=SC2012
+#shellcheck disable=
 #
-# find newest file already backed up &
-# compare it to phones photos & copy newer
-#
-# TODO: find better way to copy/sync files
+# use last_update.log to compare time & copy newer files from phone
 
+bc_dir="/g/Aza/N3/Camera"
+temp_dir="./temp"
+last_update_log="$bc_dir/last_update.log"
+declare -a folders=(dcim/Camera Download Pictures) # folders to copy files from
 
-# adb not in path, run here
-cd ~/.android/platform-tools/ || exit 1
-rm -rf ./temp && mkdir ./temp
+# adb not in path, run here & clean up old junk
+cd ~/.android/platform-tools/ || {
+  echo 'no adb in ~/.android/platform-tools/'
+  exit 1
+}
+rm -rf "$temp_dir" && mkdir "$temp_dir"
 
-declare -a folders=(dcim/Camera Download Pictures)
+if [[ ! -e $last_update_log ]]; then
+  echo -e "\n$last_update_log missing... creating it & setting mtime '6 months ago'"
+  touch -d "6 months ago" "$last_update_log"
+fi
+echo ''
 
 for folder in "${folders[@]}"; do
   # adb doesnt work straight from script in git-bash
-  # -a preserve timestamps, creates folders to ./temp
-  cmd.exe /c "adb pull -a /sdcard/$folder ./temp/${folder#*/}" || exit 1
+  # -a preserve timestamps, creates folders to temp_dir
+  cmd.exe /c "adb pull -a /sdcard/$folder $temp_dir/${folder#*/}" || exit 1
 
-  temp_dir="./temp/$folder"
-  bc_dir="/g/Aza/N3/${folder#*/}"
-  [[ ! -e "$bc_dir" ]] && mkdir "$bc_dir"
-  newest_backed_up_file=$(ls -rt "$bc_dir" | tail -1)
-  if [[ -z $newest_backed_up_file ]]; then
-    newest_backed_up_file=$(ls -rt "$temp_dir" | head -1)
-    echo -e "\nno files in $bc_dir, using $temp_dir/$newest_backed_up_file"
-    use_temp_dir=true
-  fi
+  echo -e "\n>>> $(basename "$last_update_log") mtime - $(stat -c %y "$last_update_log" | cut -d'.' -f1)"
 
-  check_file="$bc_dir/$newest_backed_up_file"
-  if [[ $use_temp_dir ]]; then
-    check_file="$temp_dir/$newest_backed_up_file"
-  fi
-  echo -e "\n$newest_backed_up_file - $(stat -c %y "$check_file" | cut -d'.' -f1) <<< newest_backed_up_file"
+  files_to_copy=()
+  while IFS= read -r -d $'\0'; do
+    files_to_copy+=("$REPLY")
+  done < <(find "$temp_dir" -type f -newer "$last_update_log" -print0)
 
   counter=0
-  for file in "$temp_dir"/*; do
-    if [[ $file -nt $check_file ]]; then
+  for file in "${files_to_copy[@]}"; do
+    if [[ ! -e "$bc_dir/$(basename "$file")" ]]; then
+      cp -np "$file" "$bc_dir/$(basename "$file")"
       ((counter += 1))
-      echo "$(basename "$file") - $(stat -c %y "$file" | cut -d'.' -f1)"
-      cp -np --recursive "$file" "$bc_dir"/"$(basename "$file")"
     fi
   done
-  echo -e "\n$counter new files copied to $bc_dir"
-  echo -e "------------------------------------\n"
+
+  echo -e "\n$counter files copied to $bc_dir..."
+  echo -e "-------------------------------------\n"
 
 done
 
-rm -rf ./temp \
-  && echo "Removed ~/.android/platform-tools/temp folder" \
-  || echo "Couldn't remove ~/.android/platform-tools/temp folder"
+rm -rf "$temp_dir" &&
+  echo "Removed ~/.android/platform-tools/temp folder" ||
+  echo "Couldn't remove ~/.android/platform-tools/temp folder"
+
+echo "last runtime $(date +'%F %T')" | tee -a "$last_update_log"

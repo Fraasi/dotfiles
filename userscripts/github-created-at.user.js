86a5ebbb2d9ced6197d8e328899aad504eb2de17
Fraasi <fraasi.gh@gmail.com>
2022-08-10 16:58:45 +0300

    add soft-nav listener

--- userscripts/github-created-at.user.js
+++ userscripts/github-created-at.user.js
@@ -6,7 +6,7 @@
 // @author      Fraasi
 // @namespace   https://github.com/Fraasi
 // @match       https://github.com/*
-// @run-at      document-idle
+// @run-at      document-start
 // @grant       none
 // @icon        https://github.githubassets.com/pinned-octocat.svg
 // ==/UserScript==
@@ -24,25 +24,35 @@
     return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f];
   }
 
-  const span = document.createElement('span');
-  span.className = 'Label Label--secondary v-align-middle mr-1';
-  span.innerText = 'Created at';
-  span.style.cursor = 'pointer';
-  span.onclick = function () {
-    if (window.location.pathname.split('/').length < 3) {
-      alert('Not in a repository page.');
-      return;
-    }
-    const cleanRepoName = window.location.pathname.split('/').splice(0, 3).join('/');
-    fetch(`https://api.github.com/repos${cleanRepoName}`)
-      .then(data => data.json())
-      .then(json => {
-        console.log('Github repo alert json', json);
-        alert(`${cleanRepoName}\n\nCreated: ${new Date(json.created_at).toLocaleString('EN-GB')}\n\nSize: ${formatBytes(json.size * 1024)}`)
-      }).catch(e => alert(`${e.message}.\nSee console for more info.`));
-  };
+  function init() {
+    if (document.querySelector('.createdAt')) return;
+    const span = document.createElement('span');
+    span.className = 'Label Label--secondary v-align-middle mr-1 createdAt';
+    span.innerText = 'Created at';
+    span.style.cursor = 'pointer';
+    span.onclick = function () {
+      if (window.location.pathname.split('/').length < 3) {
+        alert('Not in a repository page.');
+        return;
+      }
+      const cleanRepoName = window.location.pathname.split('/').splice(0, 3).join('/');
+      fetch(`https://api.github.com/repos${cleanRepoName}`)
+        .then(data => data.json())
+        .then(json => {
+          console.log('Github repo alert json', json);
+          alert(`${cleanRepoName}\n\nCreated: ${new Date(json.created_at).toLocaleString('EN-GB')}\n\nSize: ${formatBytes(json.size * 1024)}`)
+        }).catch(e => alert(`${e.message}.\nSee console for more info.`));
+    };
 
-  const wrapper = document.querySelector('div.d-flex.flex-wrap.flex-items-center.wb-break-word.f3.text-normal');
-  wrapper.append(span);
+    const wrapper = document.querySelector('div.d-flex.flex-wrap.flex-items-center.wb-break-word.f3.text-normal');
+    wrapper.append(span);
+  }
+
+  document.addEventListener('soft-nav:success', e => {
+    init()
+  })
+  document.addEventListener('DOMContentLoaded', e => {
+    init()
+  })
 
 })();

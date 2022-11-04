e0259ec5ede94603c50d8a81058b9a5b8f72a3f0
Fraasi <fraasi.gh@gmail.com>
2022-11-04 13:18:02 +0200

    feat: backup now using wifi connection

--- bin/backup-phone-photos.sh
+++ bin/backup-phone-photos.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #shellcheck disable=
 #
 # use last_update.log to compare time & copy newer files from phone
 
-bc_dir="/g/Aza/N3/Camera"
+bc_dir="/g/Aza/Phone/backup"
 temp_dir="./temp"
 last_update_log="$bc_dir/last_update.log"
-declare -a folders=(dcim/Camera Download Pictures) # folders to copy files from
+declare -a folders=(DCIM Download Pictures) # folders to copy files from
 
 # adb not in path, run here & clean up old junk
 cd ~/.android/platform-tools/ || {
@@ -15,23 +15,28 @@ cd ~/.android/platform-tools/ || {
 }
 rm -rf "$temp_dir" && mkdir "$temp_dir"
 
+# connect to phone or fail straight up
+IFS=' : ' read -ra gateway < <(ipconfig | tail -1)
+int_ip=${gateway[-1]}
+phone_ip=${int_ip%.*}.3
+cmd.exe /c "adb connect $phone_ip:5555" || exit 1
+
 if [[ ! -e $last_update_log ]]; then
   echo -e "\n$last_update_log missing... creating it & setting mtime '6 months ago'"
   touch -d "6 months ago" "$last_update_log"
 fi
-echo ''
+
+echo -e "\n>>> $(basename "$last_update_log") mtime - $(stat -c %y "$last_update_log" | cut -d'.' -f1)\n"
 
 for folder in "${folders[@]}"; do
   # adb doesnt work straight from script in git-bash
   # -a preserve timestamps, creates folders to temp_dir
   cmd.exe /c "adb pull -a /sdcard/$folder $temp_dir/${folder#*/}" || exit 1
 
-  echo -e "\n>>> $(basename "$last_update_log") mtime - $(stat -c %y "$last_update_log" | cut -d'.' -f1)"
-
   files_to_copy=()
   while IFS= read -r -d $'\0'; do
     files_to_copy+=("$REPLY")
-  done < <(find "$temp_dir" -type f -newer "$last_update_log" -print0)
+  done < <(find "$temp_dir" -type f -not -path "*thumbnail*" -newer "$last_update_log" -print0)
 
   counter=0
   for file in "${files_to_copy[@]}"; do
@@ -50,4 +55,5 @@ rm -rf "$temp_dir" &&
   echo "Removed ~/.android/platform-tools/temp folder" ||
   echo "Couldn't remove ~/.android/platform-tools/temp folder"
 
-echo "last runtime $(date +'%F %T')" | tee -a "$last_update_log"
+echo -e "\n>>> $(basename "$last_update_log") mtime now at - $(date +'%F %T')" | tee -a "$last_update_log"
+

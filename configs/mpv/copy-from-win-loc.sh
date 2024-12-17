7fb3aeeb972f1a0da90c6ff98d6d85f03367f48d
fraasi <fraasi.gh@gmail.com>
2024-12-17 16:14:23 +0200

    add mpv confs

---
+++ configs/mpv/copy-from-win-loc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+cp -i /c/Users/fraasi/AppData/Roaming/mpv/input.conf .
+cp -i /c/Users/fraasi/AppData/Roaming/mpv/mpv.conf .
+cp -i /c/Users/fraasi/AppData/Roaming/mpv/script-opts/uosc.conf ./script-opts/
